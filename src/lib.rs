fe6e8feb6aebc018b8290b6c8ed42b703aacba31
ln2r <scorch_62@live.com>
2025-12-08 08:44:15 +0000

    feat - move get works to its own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
+mod works;
+
 use worker::*;
+use crate::works::get_works;
 
 #[event(fetch)]
 async fn fetch(
@@ -9,38 +12,12 @@ async fn fetch(
     let router = Router::new();
 
     router.get_async("/works",| _req, _ctx | async move {
-        let url = "https://api.github.com/users/ln2r/repos";
-
-        // request initialization
-        let mut init = RequestInit::new();
-        init.with_method(Method::Get);
-
-        // setting headers
-        let headers = Headers::new();
-        headers.set("Accept", "application/vnd.github.v3+json")?;
-        headers.set("User-Agent", "mugunghwa-cfw")?;
-        init.with_headers(headers);
-
-        let mut res = Fetch::Request(Request::new_with_init(url, &init)?)
-            .send()
-            .await?;
-
-        let data = res.json::<serde_json::Value>().await?;
-        let empty = vec![];
-        let public: Vec<_> = data
-            .as_array()
-            .unwrap_or(&empty)
-            .iter()
-            .filter(|repo| repo["visibility"] == "public" && repo["full_name"] != "ln2r/ln2r")
-            .collect();
-
-        let mut response = Response::from_json(&public)?;
-        response.headers_mut().set("Access-Control-Allow-Origin", "*")?;
-        response.headers_mut().set("Access-Control-Allow-Methods", "GET, OPTIONS")?;
-        response.headers_mut().set("Access-Control-Allow-Headers", "*")?;
-        
-        Ok(response)
+        get_works().await
     })
+        .get_async("/writings", |_req, _ctx | async move {
+
+            Response::ok("ok")
+        })
         .run(req, env)
         .await
 }
---
+++ src/works.rs
@@ -0,0 +1,36 @@
+use worker::{Fetch, Headers, Method, Request, RequestInit, Response};
+
+pub async fn get_works() -> Result<Response, worker::Error> {
+    let url = "https://api.github.com/users/ln2r/repos";
+
+    // request initialization
+    let mut init = RequestInit::new();
+    init.with_method(Method::Get);
+
+    let headers = Headers::new();
+    headers.set("Accept", "application/vnd.github.v3+json")?;
+    headers.set("User-Agent", "mugunghwa-cfw")?;
+    init.with_headers(headers);
+
+    let mut res = Fetch::Request(Request::new_with_init(url, &init)?)
+        .send()
+        .await?;
+
+    let data = res.json::<serde_json::Value>().await?;
+    let empty = vec![];
+    let public: Vec<_> = data
+        .as_array()
+        .unwrap_or(&empty)
+        .iter()
+        .filter(|repo| repo["visibility"] == "public" && repo["full_name"] != "ln2r/ln2r")
+        .collect();
+
+    let mut response = Response::from_json(&public)?;
+    response.headers_mut().set("Access-Control-Allow-Origin", "*")?;
+    response.headers_mut().set("Access-Control-Allow-Methods", "GET, OPTIONS")?;
+    response.headers_mut().set("Access-Control-Allow-Headers", "*")?;
+
+    Result::Ok(response)
+}
+
+pub async fn add_work() -> Result<Response, worker::Error> {}
