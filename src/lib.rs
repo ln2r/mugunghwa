c022d5a52e690f3adda78be13225255418f8c3fa
ln2r <scorch_62@live.com>
2025-12-09 15:49:22 +0000

    feat - integrate writings module, add api key check

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 mod works;
+mod writings;
+mod commons;
 
 use worker::*;
+use crate::commons::check_key;
 use crate::works::get_works;
+use crate::writings::{add_writings, get_writing, get_writings};
 
 #[event(fetch)]
 async fn fetch(
@@ -14,9 +18,27 @@ async fn fetch(
     router.get_async("/works",| _req, _ctx | async move {
         get_works().await
     })
-        .get_async("/writings", |_req, _ctx | async move {
+        // Writing Endpoints
+        .get_async("/writings",| _req, _ctx | async move {
+            get_writings().await
+        })
+        .get_async("/writing/:id",| req, ctx | async move {
+            if let Some(resp) = check_key(&req, &ctx)? {
+                return Ok(resp);
+            }
 
-            Response::ok("ok")
+            let id = ctx.param("id").unwrap().to_string();
+
+            return Ok(get_writing(id).await.expect("Id required"));
+        })
+        .post_async("/writing", |mut req, ctx | async move {
+            if let Some(resp) = check_key(&req, &ctx)? {
+                return Ok(resp);
+            }
+
+            let body = req.json().await?;
+
+            return Ok(add_writings(body).await.expect("Body required"));
         })
         .run(req, env)
         .await
