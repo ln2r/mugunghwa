94212ef56509125cbfcc7dce9839d56d248749ba
ln2r <scorch_62@live.com>
2025-12-13 05:56:38 +0000

    feat - actually returning writings data on get, update time format

--- src/writings.rs
+++ src/writings.rs
@@ -13,33 +13,41 @@ pub struct Writing {
     pub deleted: Option<String>,
 }
 
-pub async fn get_writings() -> Result<Response, worker::Error> {
-    Result::Ok(Response::ok(
-        "OK"
-    )?)
+pub async fn get_writings(ctx: &RouteContext<()>) -> Result<Response, worker::Error> {
+    let d1 = ctx.env.d1(&ctx.env.var("db_name")?.to_string())?;
+
+    let res = d1.prepare("SELECT * FROM writings WHERE deleted IS NULL;")
+        .all()
+        .await?;
+
+    let writings: Vec<Writing> = res.results()?;
+
+    Result::Ok(Response::from_json(&writings)?)
 }
 
-pub async fn get_writing(id: String) -> Result<Response, worker::Error> {
-    console_log!("{:?}", id);
+pub async fn get_writing(id: String, ctx: &RouteContext<()>) -> Result<Response, worker::Error> {
+    let d1 = ctx.env.d1(&ctx.env.var("db_name")?.to_string())?;
 
-    Result::Ok(Response::ok(
-        "OK"
-    )?)
+    let res = d1.prepare("SELECT * FROM writings WHERE id = ? AND deleted IS NULL;")
+        .bind(&[
+            JsValue::from(&id.to_string()),
+        ])?
+        .first::<Writing>(None)
+        .await?;
+
+    Result::Ok(Response::from_json(&res)?)
 }
 
 pub async fn add_writings(body: Writing, ctx: &RouteContext<()>) -> Result<Response, worker::Error> {
     let d1 = ctx.env.d1(&ctx.env.var("db_name")?.to_string())?;
 
     let id = generate_snowflake(ctx);
-    let now = Date::now();
 
-    d1.prepare("INSERT INTO writings (id, title, body, created, updated) VALUES (?, ?, ?, ?, ?);")
+    d1.prepare("INSERT INTO writings (id, title, body, created, updated) VALUES (?, ?, ?, strftime('%Y-%m-%dT%H:%M:%fZ','now'), strftime('%Y-%m-%dT%H:%M:%fZ','now'));")
         .bind(&[
             JsValue::from(&id.to_string()),
             JsValue::from(&body.title.to_string()),
             JsValue::from(&body.body.to_string()),
-            JsValue::from(now.to_string()),
-            JsValue::from(now.to_string()),
         ])?
         .run()
         .await?;
@@ -60,11 +68,10 @@ pub async fn update_writing(body: Writing, ctx: &RouteContext<()>) -> Result<Res
 
     let now = Date::now();
 
-    d1.prepare("UPDATE writings SET title = ?, body = ?, updated = ? WHERE id = ?;")
+    d1.prepare("UPDATE writings SET title = ?, body = ?, updated = strftime('%Y-%m-%dT%H:%M:%fZ','now') WHERE id = ?;")
         .bind(&[
             JsValue::from(&body.title.to_string()),
             JsValue::from(&body.body.to_string()),
-            JsValue::from(now.to_string()),
             JsValue::from(&body.id.to_string()),
         ])?
         .run()
