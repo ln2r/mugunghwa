ddeafe9581ec7a4e8237fca026190a4a93c7fec5
ln2r <scorch_62@live.com>
2026-04-05 16:07:59 +0000

    feat: migrate previous migrations

---
+++ migrations/0002_add-slug-to-writings.sql
@@ -0,0 +1,3 @@
+-- Migration number: 0002 	 2025-12-13T17:56:45.571Z
+ALTER TABLE writings
+    ADD COLUMN slug TEXT;
---
+++ migrations/0003_add-hero-to-writings.sql
@@ -0,0 +1,3 @@
+-- Migration number: 0003 	 2025-12-13T18:02:07.262Z
+ALTER TABLE writings
+    ADD COLUMN hero TEXT;
