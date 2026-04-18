6c84bd47c4670c476f4ef6fac153fcb2c4728203
ln2r <scorch_62@live.com>
2026-04-18 13:20:14 +0000

    feat: add user table

---
+++ src/auth/model.ts
@@ -0,0 +1,10 @@
+export interface Users {
+    id?: string;
+    username: string;
+    avatar?: string;
+    url?: string;
+    provider_id: string;
+    created?: string;
+    updated?: string;
+    deleted?: string;
+}
